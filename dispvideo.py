3861d201a82cb010d8f7fe7bc9dacbdd8f644170
Eric Silk <eric.silk@ericsilk.com>
2020-02-18 07:35:08 -0800

    Grayscale finished, thresholding added

--- dispvideo.py
+++ dispvideo.py
@@ -5,58 +5,59 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from skimage import color
 
-r_index = 0
-g_index = 1
-b_index = 2
+RED = 0
+GREEN = 1
+BLUE = 2
 
-def get_red(x):
-    x[:,:,1,:] = 0
-    x[:,:,2,:] = 0
-    return x
-
-def get_green(x):
-    x[:,:,0,:] = 0
-    x[:,:,2,:] = 0
-    return x
-
-def get_blue(x):
-    x[:,:,0,:] = 0
-    x[:,:,1,:] = 0
-    return x
+def get_channel(x, channel):
+    ret = np.copy(x)
+    return np.squeeze(ret[:, :, channel, :])
 
 def grayscale(x):
-    red = np.squeeze(get_red(x)[:, :, r_index, :])
-    blue = np.squeeze(get_blue(x)[:, :, g_index, :])
-    green = np.squeeze(get_green(x)[:, :, b_index, :])
+
+    red = get_channel(x, RED)
+    green = get_channel(x, GREEN)
+    blue = get_channel(x, BLUE)
 
     gray = 0.2125 * red + 0.7154 * green + 0.0721 * blue
 
-    print("red:", red[:].max())
-    print("blue:", blue[:].max())
-    print("green:", green[:].max())
-    print("gray:", gray[:].max())
+    print("red:", red.max())
+    print("blue:", blue.max())
+    print("green:", green.max())
+    print("gray:", gray.max())
 
     return gray
 
-def play_video(four_dim_array):
+def apply_threshold(x, thresh):
+    ret = np.copy(x)
+    indices = x <= thresh
+    ret[indices] = 0
+
+    return ret
+
+def play_video(four_dim_array, threshold):
     """
     Assumes the input is [y, x, RGB, sample], plays the video
     """
     _, _, _, samples = four_dim_array.shape
-    #channel = blue(four_dim_array)
     gray = grayscale(four_dim_array)
+    thresholded = apply_threshold(gray, threshold)
     f = plt.figure(1)
     f.canvas.manager.window.activateWindow()
     f.canvas.manager.window.raise_()
     p = f.add_subplot(111)
-    print("Gray shape:", gray.shape)
-    image = p.imshow(gray[:, :, 0], cmap=plt.get_cmap("gray"))
+    image = p.imshow(thresholded[:, :, 0], cmap="gray", vmin=0, vmax=255)
     for i in range(1, samples):
-        tmp = gray[:, :, i]
+        tmp = thresholded[:, :, i]
         image.set_data(tmp)
         plt.pause(0.02)
 
-def main(fname):
+def main(fname, threshold):
+    try:
+        threshold = int(threshold)
+    except TypeError:
+        print("Second argument must be convertible to integer!")
+        return
 
     video = scipy.io.loadmat(fname)
     common_keys = ["__header__", "__version__", "__globals__"]
@@ -68,9 +69,12 @@ def main(fname):
     video = video[key]
     print("Read video mat file.")
 
-    play_video(video)
+    play_video(video, threshold)
 
 if __name__ == "__main__":
     fname = sys.argv[1]
-    print("File to play:", fname)
-    main(fname)
+    try:
+        threshold = sys.argv[2]
+    except IndexError:
+        threshold=0
+    main(fname, threshold)
