960d7789bac262e581e11d633f3334c2becb0012
Eric Silk <eric_silk@selinc.com>
2020-02-19 13:32:58 -0800

    Tracking and drawn boxes working

--- cv2test.py
+++ cv2test.py
@@ -18,20 +18,23 @@ for frame in x:
         initBB = cv2.selectROI("test", frame, fromCenter=False, showCrosshair=True)
         tracker.init(frame, initBB)
 
-    if initBB:
-        success, box = tracker.update(frame)
-        
-        if success:
-            x, y, w, h = [int(v) for v in box]
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        info = [("Success", "Yes" if success else "No")]
+    success, box = tracker.update(frame)
+    
+    if success:
+        x, y, w, h = [int(v) for v in box]
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2, 1)
+    info = [("Success", "Yes" if success else "No")]
+    p1 = (box[0], box[1])
+    p2 = (box[0]+box[2], box[1]+box[3])
+    #cv2.rectangle
 
-        for (i, (k, v)) in enumerate(info):
-            text = "{}: {}".format(k, v)
-            cv2.putText(frame, text, (10, H - ((i*20)+20)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-            print(text)
+    for (i, (k, v)) in enumerate(info):
+        text = "{}: {}".format(k, v)
+        cv2.putText(frame, text, (10, H - ((i*20)+20)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+        print(text)
 
+    cv2.imshow("test", frame)
     key = cv2.waitKey(30) & 0xFF
     if key == ord("q"):
         break
